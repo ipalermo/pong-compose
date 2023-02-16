2a50d8aa8e96e25f88b73eaeda3f9cb53c5dd503
Ignacio Palermo <nacho.palermo84@gmail.com>
2023-02-16 11:17:54 +0000

    Add ball-edge collision detection.

--- app/src/main/java/com/ipalermo/pong/feature/board/BoardViewModel.kt
+++ app/src/main/java/com/ipalermo/pong/feature/board/BoardViewModel.kt
@@ -59,11 +59,11 @@ class BoardViewModel : ViewModel() {
     )
 
     private fun updateBall() = _boardState.update { state ->
-        val nextDirection =
-            if (state.ballRacketCollision) state.ball.direction.opposite() else state.ball.direction
+        val nextDirection = if (state.ballRacketCollision) state.ball.direction.opposite()
+            else state.ball.direction
         val newXPos = state.ball.position.x + 1.dp / state.nextSlope
-        val nexYPos =
-            state.ball.position.y + if (nextDirection == Direction.Down) state.ball.speed else state.ball.speed * -1
+        val nexYPos = state.ball.position.y + if (nextDirection == Direction.Down) state.ball.speed
+            else state.ball.speed * -1
         val newPosition = DpOffset(newXPos, nexYPos)
         state.copy(
             ball = state.ball.copy(
@@ -73,6 +73,9 @@ class BoardViewModel : ViewModel() {
             )
         )
     }
+
+    private fun BoardState.ballBoardEdgeCollision() = ball.position.x - ball.radius <= 0f.dp
+            || ball.position.x + ball.radius >= boardSize.width
 
     private fun Direction.opposite() = if (this == Direction.Up) Direction.Down else Direction.Up
 }
@@ -91,6 +94,9 @@ data class BoardState(
         ball.position.y - ball.radius <= topRacket.topLeftY + topRacket.size.height
                 && ball.position.x in topRacket.topLeft.x..(topRacket.topLeft.x + topRacket.size.width)
 
+    private val ballBoardEdgeCollision = ball.position.x - ball.radius <= 0f.dp
+    || ball.position.x + ball.radius >= boardSize.width
+
     val nextSlope = if (bottomRacketCollision) {
         val slopeInclination =
             if (bottomRacket.center.x.value - ball.position.x.value >= 0) -1f else 1f
@@ -104,15 +110,20 @@ data class BoardState(
             if (topRacket.center.x.value - ball.position.x.value >= 0) -1f else 1f
         val distanceToRacketCenter =
             kotlin.math.abs(topRacket.center.x.value - ball.position.x.value)
-        if (distanceToRacketCenter <= topRacket.size.width.value / 8) (1000f * slopeInclination)
-        else if (distanceToRacketCenter <= topRacket.size.width.value / 4) 1f * slopeInclination
-        else 0.25f * slopeInclination
+        if (distanceToRacketCenter <= topRacket.size.width.value / 8) (hitInCenterSlope * slopeInclination)
+        else if (distanceToRacketCenter <= topRacket.size.width.value / 4) hitAlmostInCenter * slopeInclination
+        else hitOnEdge * slopeInclination
+    } else if (ballBoardEdgeCollision) {
+        ball.slope * -1
     } else ball.slope
 
     val ballRacketCollision = bottomRacketCollision || topRacketCollision
 
     companion object {
         val Initial = BoardState()
+        const val hitInCenterSlope = 1000f
+        const val hitAlmostInCenter = 1f
+        const val hitOnEdge = 0.25f
     }
 }
 
